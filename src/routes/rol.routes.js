8d7f40358e476a5c6de4c250cdaf93fe72acb0fe
JavierP721 <javiergomez3175@gmail.com>
2024-12-22 23:48:23 -0600

    modificacion rol.route

--- src/routes/rol.routes.js
+++ src/routes/rol.routes.js
@@ -8,12 +8,3 @@ router.put('/:id', authMiddleware, RolController.updateRol);
 router.get('/', authMiddleware, RolController.getAllRoles);
 
 module.exports = router;const express = require('express');
-const router = express.Router();
-const RolController = require('../src/controllers/rol.controller.js');
-const authMiddleware = require('../src/middleware/auth.middleware.js');
-
-router.post('/', authMiddleware, RolController.createRol);
-router.put('/:id', authMiddleware, RolController.updateRol);
-router.get('/', authMiddleware, RolController.getAllRoles);
-
-module.exports = router;
